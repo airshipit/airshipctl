d5e04985466b8f4b3a4784004c14c262666cc14a
Vladislav Kuzmin <vkuzmin@mirantis.com>
2020-05-18 16:55:01 +0400

    Improve config package organization pt.3
    This is a series of patches that refactor config.go into
    several smaller modules, each relating specifically
    to one component of the configuration structure.
    
    This commit moves the types in the modules where they are used.
    
    Change-Id: I5bbeb765d558f02e04ebf8e41987a436874b83d4
    Closes: #35

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -32,6 +32,55 @@ import (
 	"opendev.org/airship/airshipctl/pkg/util"
 )
 
+// Where possible, json tags match the cli argument names.
+// Top level config objects and all values required for proper functioning are not "omitempty".
+// Any truly optional piece of config is allowed to be omitted.
+
+// Config holds the information required by airshipctl commands
+// It is somewhat a superset of what a kubeconfig looks like, we allow for this overlaps by providing
+// a mechanism to consume or produce a kubeconfig into / from the airship config.
+type Config struct {
+	// +optional
+	Kind string `json:"kind,omitempty"`
+
+	// +optional
+	APIVersion string `json:"apiVersion,omitempty"`
+
+	// Clusters is a map of referenceable names to cluster configs
+	Clusters map[string]*ClusterPurpose `json:"clusters"`
+
+	// AuthInfos is a map of referenceable names to user configs
+	AuthInfos map[string]*AuthInfo `json:"users"`
+
+	// Contexts is a map of referenceable names to context configs
+	Contexts map[string]*Context `json:"contexts"`
+
+	// Manifests is a map of referenceable names to documents
+	Manifests map[string]*Manifest `json:"manifests"`
+
+	// CurrentContext is the name of the context that you would like to use by default
+	CurrentContext string `json:"currentContext"`
+
+	// Management configuration defines management information for all baremetal hosts in a cluster.
+	ManagementConfiguration map[string]*ManagementConfiguration `json:"managementConfiguration"`
+
+	// BootstrapInfo is the configuration for container runtime, ISO builder and remote management
+	BootstrapInfo map[string]*Bootstrap `json:"bootstrapInfo"`
+
+	// loadedConfigPath is the full path to the the location of the config
+	// file from which this config was loaded
+	// +not persisted in file
+	loadedConfigPath string
+
+	// kubeConfigPath is the full path to the the location of the
+	// kubeconfig file associated with this airship config instance
+	// +not persisted in file
+	kubeConfigPath string
+
+	// Private instance of Kube Config content as an object
+	kubeConfig *clientcmdapi.Config
+}
+
 // LoadConfig populates the Config object using the files found at
 // airshipConfigPath and kubeConfigPath
 func (c *Config) LoadConfig(airshipConfigPath, kubeConfigPath string) error {
