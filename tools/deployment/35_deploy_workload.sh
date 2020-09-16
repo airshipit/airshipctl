eb40a5700f1f465d98e7989d9f44a1f082d4bf7b
Sean Eagan <seaneagan1@gmail.com>
2020-09-16 15:39:02 -0500

    Add gating for helm release management
    This adds gating for helm release management by including a minimal
    example workload phase which consists of a HelmRelease for the nginx
    ingress controller and a corresponding deployment script including
    supporting validation logic.
    
    Change-Id: Ia21a799030289c7e40a0e61292578987ea0f6c63
    Relates-To: #351

---
+++ tools/deployment/35_deploy_workload.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -xe
+
+#Default wait timeout is 600 seconds
+export TIMEOUT=${TIMEOUT:-600s}
+export KUBECONFIG=${KUBECONFIG:-"$HOME/.airship/kubeconfig"}
+export KUBECONFIG_TARGET_CONTEXT=${KUBECONFIG_TARGET_CONTEXT:-"target-cluster"}
+export TARGET_IP=${TARGET_IP:-"10.23.25.102"}
+export TARGET_PORT=${TARGET_PORT:-"30000"}
+
+echo "Deploy workload"
+airshipctl phase run  workload-target --debug
+
+# TODO: Remove this after migrating to HelmRelease v2 (helm-controller) which
+#      supports a Ready status condition, which `airshipctl phase run` above will consume.
+echo "Waiting $TIMEOUT for HelmReleases to become ready."
+kubectl --timeout $TIMEOUT --kubeconfig $KUBECONFIG --context $KUBECONFIG_TARGET_CONTEXT wait hr --for condition=released --all --all-namespaces
+
+echo "Ensure we can reach ingress controller default backend"
+if "404" != "$(curl --head --write-out '%{http_code}' --silent --output /dev/null $TARGET_IP:$TARGET_PORT/should-404)"; then
+    echo -e "\nFailed to reach ingress controller default backend."
+    exit 1
+fi
